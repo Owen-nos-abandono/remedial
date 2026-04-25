02b986c3253cc737730580703798c8674e73f219
Janine Rivera <2022371117@uteq.edu.mx>
2026-04-24 18:00:37 -0600

    Prueba de que sirve esto

--- k6-script.js
+++ k6-script.js
@@ -2,6 +2,6 @@ import http from 'k6/http';
 import { sleep } from 'k6';
 
 export default function () {
-  http.get('http://34.51.71.130'); // Tu IP pública
+  http.get('http://34.51.71.130'); //ip
   sleep(1);
 }
